ae1ca3a360f6f8e82072f593f149cfbe66cd0971
uliana arruda <uliana.paiva@hotmail.com>
2024-11-13 18:08:00 -0300

    teste nagativos e rooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,8 +2,16 @@
 
 describe(' funcionalidade login' ,() => {
 
-    it('Deve fazer login com sucesso',() => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso',() => {
         cy.get('#username').type('uliana.paiva@hotmail.com')
         cy.get('#password').type('matteus152030')
         cy.get('.woocommerce-form > .button').click()
@@ -11,4 +19,23 @@ describe(' funcionalidade login' ,() => {
     
 
     })
+
+
+it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
+    cy.get('#username').type('uli.paiva@hotmail.com')
+    cy.get('#password').type('matteus152030')
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-error').should('exist')   
+
+    
+});
+
+it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('uliana.paiva@hotmail.com')
+        cy.get('#password').type('152030')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain','Erro: A senha fornecida para o e-mail uliana.paiva@hotmail.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('exist')
+    
+});
 })
